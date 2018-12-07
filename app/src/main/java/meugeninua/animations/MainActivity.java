566919df4258e996c6b2ddd147154fea5696e931
Yevgeniy Malyshev <yevhen.malyshev@sixt.com>
2018-12-07 11:54:38 +0200

    Implemented AnimationDrawable

--- app/src/main/java/meugeninua/animations/MainActivity.java
+++ app/src/main/java/meugeninua/animations/MainActivity.java
@@ -1,13 +1,51 @@
 package meugeninua.animations;
 
+import android.content.res.Resources;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AnimationDrawable drawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.image);
+        drawable = createAnimationDrawable();
+        imageView.setImageDrawable(drawable);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        drawable.start();
+    }
+
+    private AnimationDrawable createAnimationDrawable() {
+        Resources resources = getResources();
+
+        int[] items = new int[] {
+                R.drawable.baseline_battery_charging_20_96,
+                R.drawable.baseline_battery_charging_30_96,
+                R.drawable.baseline_battery_charging_50_96,
+                R.drawable.baseline_battery_charging_60_96,
+                R.drawable.baseline_battery_charging_80_96,
+                R.drawable.baseline_battery_charging_90_96,
+                R.drawable.baseline_battery_charging_full_96,
+                R.drawable.baseline_battery_full_96};
+        AnimationDrawable drawable = new AnimationDrawable();
+        drawable.setOneShot(false);
+        for (int item : items) {
+            drawable.addFrame(
+                    VectorDrawableCompat.create(resources, item, getTheme()),
+                    resources.getInteger(R.integer.short_duration));
+        }
+        return drawable;
     }
 }
